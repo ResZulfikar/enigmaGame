2228553b2c560bbdd761ee05fc137f015e37db44
resZulfikar <resazulf@gmail.com>
2020-06-26 17:58:37 +0700

    commit resa

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    private Rigidbody2D myRigidBody;
+    private Animator myAnim1;
+    private Collider2D myCollider;
+    public float kecLoncat = 500f;
+
+    //animasi mati
+    private float bunnyHurtTime = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnim1 = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bunnyHurtTime == -1)
+        {
+
+            //gerakan setelah space ditekan
+            if (Input.GetButtonUp("Jump"))
+            {
+                myRigidBody.AddForce(transform.up * kecLoncat);
+            }
+
+            //animator set velocity karakter terhadap sumbu y
+            myAnim1.SetFloat("vVelocity", myRigidBody.velocity.y);
+        }
+        else
+        {
+            if(Time.time > bunnyHurtTime + 1)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+                
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            foreach(PrefabsSpawner spawner in FindObjectsOfType<PrefabsSpawner>())
+            {
+                spawner.enabled = false;
+            }
+
+            foreach(GerakObjekKiri gerakKiri in FindObjectsOfType<GerakObjekKiri>())
+            {
+                gerakKiri.enabled = false;
+            }
+
+            bunnyHurtTime = Time.time + 1;
+            myAnim1.SetBool("bunnyHurt", true);
+
+            myRigidBody.velocity = Vector2.zero;
+            myRigidBody.AddForce(transform.up * kecLoncat);
+            myCollider.enabled = false;
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/GerakObjekKiri.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerakObjekKiri : MonoBehaviour
+{
+    //konsep: setiap perubahan frame, virus bergerak ke kiri
+
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mengubah posisi virus, ke kiri (modif sumbu y aja)
+        //used Vector3 cuz transform has x,y,z values
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
