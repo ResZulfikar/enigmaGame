92a60e581003bd0b7da307269646ec437363bf4a
ResZulfikar <resazulf@gmail.com>
2020-07-02 20:01:48 +0700

    multiplayer added
    menambah dual player dan control playernya

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player1 : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        //exit ke main menu
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.LoadLevel("Title");
+        }
+    }
+
+    private void Move()
+    {
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
